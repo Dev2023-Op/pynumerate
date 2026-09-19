b90c25d7d95606f180c433993329a273c7185c4a
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2026-09-19 18:52:06 +1000

    fix everything

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import requests
+from fake_useragent import UserAgent
+ua = UserAgent()
 
 subdoms = []
-target = ""
+target = "torn.com"
 
 def subdomains():
 	global subdoms
 	global target
 	with open("subdomains.txt", "r") as file:
 		wordlist = file.read().splitlines()
+	headers = {
+		"User-Agent": ua.random
+	}
 	for item in wordlist:
-		r = request.get("https://" + item + "." + target)
-		if r.status_code == 301 or r.status_code == 200:
-			print("[*] Subdomain Found: https://" + item + "." + target)
+		try:
+			r = requests.request(method="GET", url="https://" + item + "." + target, headers=headers)
+		except requests.exceptions.RequestException:
+			pass
+		try:
+			
+			if r != "":
+				if "404" not in r:
+					print(f"[*] Subdomain Found: https://{item}.{target}")
+		except UnboundLocalError as e:
+			pass
+		r = ""
+
+subdomains()
