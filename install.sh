da15b2edf8a8b56e2d22d63e87f0ab2ac525761a
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2026-09-20 12:47:51 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cd /usr/lib
-sudo git clone https://github.com/Dev2023-Op/pynumerate.git
-cd pynumerate
-sudo python3 -m venv .venv
-source .venv/bin/activate
-sudo pip install requests
-sudo mv /usr/lib/pynumerate/pynumerate /usr/bin/pynumerate
+sudo git clone https://github.com/Dev2023-Op/pynumerate.git && cd pynumerate && sudo python3 -m venv .venv && source .venv/bin/activate && sudo pip install requests $$ echo "finnished"
