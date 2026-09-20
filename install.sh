aab39726b6edd55afb605efe09527baf2ef10989
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2026-09-20 14:16:36 +1000

    Delete install.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo git clone https://github.com/Dev2023-Op/pynumerate.git && cd pynumerate && sudo python3 -m venv .venv && source .venv/bin/activate && sudo pip install requests $$ echo "finnished"
