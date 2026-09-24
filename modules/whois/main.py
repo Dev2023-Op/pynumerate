6b949091868c13fca7c57feb36c8a1e13c6f3446
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2026-09-24 12:11:12 +1000

    fix some errors

--- modules/whois/main.py
+++ modules/whois/main.py
@@ -1,11 +1,18 @@
 import whois
+import sys
 
-try:
-	target = sys.argv[1]
-except IndexError:
-	target = input("Whats the target URL? ")
+def main():
+	try:
+		target = sys.argv[1]
+	except IndexError:
+		target = input("Whats the target URL? ")
+	try:
+		scan = whois.whois(target)
+	except whois.exceptions.WhoisError:
+		print("Invalid URL")
+	print(scan)
 
-scan = whois.whois(target)
+main()
 
 if __name__ == "__main__":
 	main()
